71511f54cdb640177147bed4bfed54cc0db86947
ShiWei Hong <hswhong96@gmail.com>
2023-02-27 15:18:05 +0000

    Added columns and data types to user model

---
+++ db/migrate/20230227151339_add_first_name_to_user.rb
@@ -0,0 +1,9 @@
+class AddFirstNameToUser < ActiveRecord::Migration[7.0]
+  def change
+    add_column :users, :first_name, :string
+    add_column :users, :last_name, :string
+    add_column :users, :task, :string
+    add_column :users, :hourly_price, :float
+    add_column :users, :location, :string
+  end
+end
