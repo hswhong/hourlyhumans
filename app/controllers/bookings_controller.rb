610570b9dff724f7e7cbd2bdcfd4461a2ffc48ba
ShiWei Hong <hswhong96@gmail.com>
2023-02-27 17:15:48 +0000

    Added def create into bookings controller

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,20 @@
 class BookingsController < ApplicationController
+  def index
+  end
+
+  def create
+    @user = current_user
+    @booking = Booking.new(booking_params)
+    @booking.user = @user
+    if @booking.save
+      redirect_to user_path(@user)
+    else
+      render :new, status: :unprocessable_entity
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:start_date, :end_date, :total_price, :user_bookee_id, :user_booker_id)
+  end
 end
