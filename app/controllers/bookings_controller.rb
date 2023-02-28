32fa9f8769793574563eb8cb1fbf0e94dc175235
Sam Nixon samnixon87@gmail.com <samnixon87@gmail.com>
2023-02-28 11:56:42 +0000

    Added create and new to Bookings. Added a form to test it.

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -2,19 +2,27 @@ class BookingsController < ApplicationController
   def index
   end
 
+  def show
+  end
+
+  def new
+    @booking = Booking.new
+  end
+
   def create
     @user = current_user
     @booking = Booking.new(booking_params)
-    @booking.user = @user
+    @booking.user_booker_id = @user.id
     if @booking.save
-      redirect_to user_path(@user)
+      redirect_to booking(@booking)
     else
       render :new, status: :unprocessable_entity
+    end
   end
 
   private
 
   def booking_params
-    params.require(:booking).permit(:start_date, :end_date, :total_price, :user_bookee_id, :user_booker_id)
+    params.require(:booking).permit(:start_date, :end_date, :total_price, :user_bookee_id, :description)
   end
 end
