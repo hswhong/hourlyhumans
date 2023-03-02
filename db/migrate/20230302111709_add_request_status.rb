166b4ae7dd7db46cbdce51579382fd35aa7b67e3
Sam Nixon samnixon87@gmail.com <samnixon87@gmail.com>
2023-03-02 12:38:40 +0000

    Tidy up redundant migrations so folks don't have to use them

--- db/migrate/20230302111709_add_request_status.rb
+++
@@ -1,5 +0,0 @@
-class AddRequestStatus < ActiveRecord::Migration[7.0]
-  def change
-    add_column :bookings, :status, :string
-  end
-end
--- db/migrate/20230302120517_remove_status_column.rb
+++
@@ -1,5 +0,0 @@
-class RemoveStatusColumn < ActiveRecord::Migration[7.0]
-  def change
-    remove_column :bookings, :status
-  end
-end
